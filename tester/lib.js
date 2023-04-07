3be5f806e527cd0edf50d0b0b7847d48692f735e
Tamas Sallai <tamas.sallai@advancedweb.hu>
2023-04-07 21:47:36 +0200

    Refactor

--- tester/lib.js
+++ tester/lib.js
@@ -1,7 +1,7 @@
-import {Observable, Subscription, EMPTY, of, from, BehaviorSubject, NEVER, defer, ReplaySubject, merge, pipe} from "rxjs";
+import {Observable, NEVER, ReplaySubject, pipe} from "rxjs";
 import {randomUUID} from "crypto";
 import {webSocket} from "rxjs/webSocket";
-import {filter, shareReplay, debounceTime, startWith, share, take, timeout, retry, map, mergeMap, tap, withLatestFrom, switchMap, first, takeUntil} from "rxjs/operators";
+import {filter, shareReplay, debounceTime, startWith, share, take, timeout, retry, map, mergeMap, tap, withLatestFrom, first, takeUntil} from "rxjs/operators";
 
 const exponentialBackoffWithJitter = ({base, cap, timeout: timeoutMs, maxAttempts}) => pipe(
 	timeout({first: timeoutMs}),
@@ -20,54 +20,47 @@ export const appsyncRealtime = ({APIURL, connectionRetryConfig = {}, WebSocketCt
 
 	const getAuthorizationHeadersSubject = new ReplaySubject(1);
 
-	const websockets = defer(() =>
-		NEVER.pipe(
-			startWith(undefined),
-			withLatestFrom(getAuthorizationHeadersSubject),
-			mergeMap(([, fn]) => Promise.resolve(fn({connect: true, data: {}}))),
-			(observable) => new Observable((subscriber) => {
-				const subscription = observable.subscribe({
-					next: (authHeaders) => {
-						const ws = webSocket({
-							url: `wss://${new URL(realtimeUrl).host}${new URL(realtimeUrl).pathname}?header=${Buffer.from(JSON.stringify(authHeaders), "utf8").toString("base64")}&payload=${Buffer.from(JSON.stringify({}), "utf8").toString("base64")}`,
-							protocol: "graphql-ws",
-							WebSocketCtor: WebSocketCtor ?? WebSocket,
-							openObserver: ({next: () => {
-								ws.next({type: "connection_init"});
-							}}),
-						});
-						subscription.add(ws.pipe(
-							filter(({type, id}) => type === "error" && !id),
-						).subscribe({
-							next: ({payload}) => subscriber.error(payload),
-							error: (e) => subscriber.error(e),
-							complete: () => subscriber.complete(),
-						}));
-						subscription.add(ws.pipe(
-							filter(({type}) => type === "connection_ack"),
-							take(1),
-							tap(() => subscriber.next(ws)),
-							mergeMap(({payload: {connectionTimeoutMs}}) => {
-								return ws
-									.pipe(
-										filter(({type}) => type === "ka"),
-										startWith(undefined),
-										debounceTime(connectionTimeoutMs),
-										tap(() => subscriber.complete()),
-									);
-							}),
-						).subscribe({
-							error: (e) => subscriber.error(e),
-							complete: () => subscriber.complete(),
-						}));
-					},
-					error: (e) => subscriber.error(e),
-					complete: () => subscriber.complete(),
-				});
-				return () => subscription.unsubscribe();
-			})
-		)
-	).pipe(
+	const websockets = NEVER.pipe(
+		startWith(undefined),
+		withLatestFrom(getAuthorizationHeadersSubject),
+		mergeMap(([, fn]) => Promise.resolve(fn({connect: true, data: {}}))),
+		(observable) => new Observable((subscriber) => {
+			const subscription = observable.subscribe({
+				next: (authHeaders) => {
+					const ws = webSocket({
+						url: `wss://${new URL(realtimeUrl).host}${new URL(realtimeUrl).pathname}?header=${Buffer.from(JSON.stringify(authHeaders), "utf8").toString("base64")}&payload=${Buffer.from(JSON.stringify({}), "utf8").toString("base64")}`,
+						protocol: "graphql-ws",
+						WebSocketCtor: WebSocketCtor ?? WebSocket,
+						openObserver: ({next: () => {
+							ws.next({type: "connection_init"});
+						}}),
+					});
+					const wsMessages = ws.pipe(
+						tap(({type, id, payload}) => type === "error" && !id && (subscriber.error(payload))),
+					);
+					subscription.add(wsMessages.pipe(
+						filter(({type}) => type === "connection_ack"),
+						take(1),
+						tap(() => subscriber.next(ws)),
+						mergeMap(({payload: {connectionTimeoutMs}}) => {
+							return wsMessages
+								.pipe(
+									filter(({type}) => type === "ka"),
+									startWith(undefined),
+									debounceTime(connectionTimeoutMs),
+									tap(() => subscriber.complete()),
+								);
+						}),
+					).subscribe({
+						error: (e) => subscriber.error(e),
+						complete: () => subscriber.complete(),
+					}));
+				},
+				error: (e) => subscriber.error(e),
+				complete: () => subscriber.complete(),
+			});
+			return () => subscription.unsubscribe();
+		}),
 		shareReplay({refCount: true}),
 	);
 
@@ -122,15 +115,13 @@ export const appsyncRealtime = ({APIURL, connectionRetryConfig = {}, WebSocketCt
 					exponentialBackoffWithJitter({base: subscriptionRetryConfig.base ?? 10, cap: subscriptionRetryConfig.cap ?? 2000, maxAttempts: subscriptionRetryConfig.maxAttempts ?? 5, timeout: subscriptionRetryConfig.timeout ?? 5000}),
 					first(),
 					tap(opened),
-					mergeMap(() => {
-						return wsSubscription.pipe(
-							takeUntil(wsSubscription.pipe(
-								filter(({type}) => type === "complete"),
-							)),
-							filter(({type}) => type === "data"),
-							map(({payload}) => payload),
-						)
-					}),
+					mergeMap(() =>  wsSubscription.pipe(
+						takeUntil(wsSubscription.pipe(
+							filter(({type}) => type === "complete"),
+						)),
+						filter(({type}) => type === "data"),
+						map(({payload}) => payload),
+					)),
 				).subscribe(observer));
 			},
 			error: (e) => observer.error(e),
